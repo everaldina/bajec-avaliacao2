e5c7ec26c26ac70cf27f9bc666c700d3fa8eb4de
Beatriz-ux <beatrizpereira.ld@gmail.com>
2023-10-23 10:14:24 -0300

    feat: chamando o método de ordenar ,apoś a entrada de dados

--- src/DataFruta.cpp
+++ src/DataFruta.cpp
@@ -152,7 +152,7 @@ class ListaSalarios : public Lista {
 			cin >> x;
 			this->lista.push_back(x);
 		}
-		//apos a entrada de dados, vou ordenar a lista
+		this->ordena();
 	}
 			
 	void mostraMediana() {
