ae01cc3dfbdda5593ff891844cff425bc823cafb
Everaldina G. Barbosa <everaldinag@gmail.com>
2023-10-23 10:00:08 -0300

    fix: descomentando testes

--- src/DataFruta.cpp
+++ src/DataFruta.cpp
@@ -239,22 +239,20 @@ int main () {
 	listaNomes.entradaDeDados();
 	listaDeListas.push_back(&listaNomes);
 	
-	/*
-		ListaDatas listaDatas;
-		listaDatas.entradaDeDados();
-		listaDeListas.push_back(&listaDatas);
-		
-		ListaSalarios listaSalarios;
-		listaSalarios.entradaDeDados();
-		listaDeListas.push_back(&listaSalarios);
-		
-		ListaIdades listaIdades;
-		listaIdades.entradaDeDados();
-		listaDeListas.push_back(&listaIdades);
-	*/
+	ListaDatas listaDatas;
+	listaDatas.entradaDeDados();
+	listaDeListas.push_back(&listaDatas);
+	
+	ListaSalarios listaSalarios;
+	listaSalarios.entradaDeDados();
+	listaDeListas.push_back(&listaSalarios);
+	
+	ListaIdades listaIdades;
+	listaIdades.entradaDeDados();
+	listaDeListas.push_back(&listaIdades);
 	
 	for (Lista* l : listaDeListas) {
-		//l->mostraMediana();
+		l->mostraMediana();
 		l->mostraMenor();
 		l->mostraMaior();
 	}
